4d5bd045f2cec04516ad5955898d5b3ee7c939bd
Richard Tzanov <richardtzanov@gmail.com>
2023-01-09 11:17:28 +0000

    --wip-- [skip ci]

---
+++ routes/about.tsx
@@ -0,0 +1,20 @@
+import { Handlers } from "$fresh/server.ts";
+
+export const handler: Handlers = {
+  async GET(req, ctx) {
+    const resp = await ctx.render();
+    resp.headers.set("X-Custom-Header", "Hello");
+    return resp;
+  },
+};  
+
+const AboutPage = () => {
+    return (
+      <main>
+        <h1>About</h1>
+        <p>This is the about page.</p>
+      </main>
+    );
+  }
+
+export default AboutPage
